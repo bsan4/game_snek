82853ed8bbc27b3e07883dd5c2bb815fbff42228
Maxime Bezanilla <maxime.bezanilla@gadz.org>
2022-08-26 21:55:23 +0200

    amélioration méchant

--- lib/components/enemy_snake.dart
+++ lib/components/enemy_snake.dart
@@ -9,11 +9,24 @@ import 'package:flutter/material.dart' hide Gradient;
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:snake_snake/components/snake.dart';
+import 'package:snake_snake/components/food_manager.dart';
 
 import 'game.dart';
 
 class EnemySnake extends ExampleSnake {
+
   EnemySnake(double radius) : super(radius) {
     ;
+  }
+
+  @override
+  void update(double dt)
+  {
+    super.update(dt);
+  }
+
+  void Vector2 _getFoodCoordinates()
+  {
+    print("[SENEMY SNAKE] @_getFoodCoordinates - position de la bouffe :  ${gameRef.foodManager.position}\n");
   }
 }
