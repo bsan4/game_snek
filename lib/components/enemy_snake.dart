60c6043706fb1f835a2b345aa0a16ca73dbf44a4
Maxime Bezanilla <maxime.bezanilla@gadz.org>
2022-08-26 22:16:36 +0200

    update enemy snek

--- lib/components/enemy_snake.dart
+++ lib/components/enemy_snake.dart
@@ -13,21 +13,37 @@ import 'package:snake_snake/components/food_manager.dart';
 
 import 'game.dart';
 
+/// EnemySnake is just like a basic snaky snake, except it is animated
+/// by a thousand years old daemon's damned soul. Watch out : it will
+/// try to eat your food and there is nothing you can do about it.
 class EnemySnake extends ExampleSnake {
-
   EnemySnake(double radius) : super(radius) {
     ;
   }
 
   @override
-  void update(double dt)
-  {
+  void update(double dt) {
+    /// Calculate the new direction, which is towards the food
     Vector2 foodCoordinates = _getFoodCoordinates();
+    if (position.x > foodCoordinates.x) {
+      setDirection(SnakeDirection.Left);
+    } else {
+      setDirection(SnakeDirection.Right);
+    }
+
+    if (position.y > foodCoordinates.y) {
+      setDirection(SnakeDirection.Up);
+    } else {
+      setDirection(SnakeDirection.Down);
+    }
+
+    ///Propagate the method
     super.update(dt);
   }
 
-  void Vector2 _getFoodCoordinates()
-  {
-    print("[SENEMY SNAKE] @_getFoodCoordinates - position de la bouffe :  ${gameRef.foodManager.position}\n");
+  Vector2 _getFoodCoordinates() {
+    print(
+        "[SENEMY SNAKE] @_getFoodCoordinates - position de la bouffe :  ${gameRef.foodManager.position}\n");
+    return gameRef.foodManager.position;
   }
 }
