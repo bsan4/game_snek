34b23368b6fdfdc2f52696cdb123b5777a20153f
MoiseDupuys <tinou33127@gmail.com>
2022-08-27 21:19:00 +0200

    Commit before firestore de mes deux

--- lib/providers/score_providers.dart
+++ lib/providers/score_providers.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
@@ -20,6 +21,7 @@ class ScoreOnline {
 class ScoreProvider with ChangeNotifier {
   late int _myScore;
   late int _myBestScore;
+  final db = FirebaseFirestore.instance;
 
   List<ScoreOnline> fakeList = [
     ScoreOnline(
@@ -47,14 +49,24 @@ class ScoreProvider with ChangeNotifier {
 
   ScoreProvider() {
     //todo chope le score en local ou en ligne si on le temps
+    // Create a new user with a first and last name
+    final user = <String, dynamic>{
+      "id": "3",
+      "name": "James",
+      "score": 120
+    };
+
+// Add a new document with a generated ID
+    db.collection("scoresfirab").add(user).then((DocumentReference doc) =>
+        print('DocumentSnapshot added with ID: ${doc.id}'));
     _myScore = 0;
     _myBestScore = 0;
   }
 
   Future<List<ScoreOnline>> bestScores() async {
-    await Future.delayed(Duration(milliseconds: 5000));
-    return fakeList;
+    // await Future.delayed(Duration(milliseconds: 5000));
 
+    return fakeList;
   }
 
   bool updateScore(int newScore) {
@@ -71,6 +83,7 @@ class ScoreProvider with ChangeNotifier {
   int get myScore {
     return _myScore;
   }
+
   int get myBestScore {
     return _myBestScore;
   }
